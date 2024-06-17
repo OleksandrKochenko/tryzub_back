85cb27865ba99120bb9e86179fd3399a8a0e6b1a
OleksandrKochenko <121250212+OleksandrKochenko@users.noreply.github.com>
2024-06-17 04:46:50 +0300

    get event query

--- controllers/eventCtrl.js
+++ controllers/eventCtrl.js
@@ -1,9 +1,39 @@
 const Event = require("../models/event");
 
 const getEvents = async (req, res, next) => {
+  let events;
   try {
-    const events = await Event.find();
-    res.json(events);
+    const { up = false, past = false, emphasize = false } = req.query;
+    const today = new Date().getTime();
+    switch (true) {
+      case emphasize !== false:
+        events = await Event.find({
+          $and: [
+            { show: true },
+            { emphasize: true },
+            { startDate: { $gte: today } },
+          ],
+        }).sort({ startDate: 1 });
+        break;
+      case up !== false:
+        events = await Event.find({
+          $and: [{ show: true }, { startDate: { $gte: today } }],
+        }).sort({ startDate: 1 });
+        break;
+      case past !== false:
+        events = await Event.find({
+          $and: [{ show: true }, { startDate: { $lt: today } }],
+        }).sort({ startDate: -1 });
+        break;
+
+      default:
+        events = await Event.find({
+          show: true,
+        }).sort({ startDate: 1 });
+        break;
+    }
+
+    res.json({ today, up, past, emphasize, events: events });
   } catch (error) {
     next(error);
   }
